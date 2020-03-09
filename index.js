8b59a69d0403635b1b49a909b7b5e6204bd1e92d
Rory-Folster <61396000+Rory-Folster@users.noreply.github.com>
2020-03-09 14:13:37 +1300

    y

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 let x = 10;
-let y = 5;6
+let y = 5;
 console.log(x + y);
 console.log(x -+ y);
 console.log(x * y);
 console.log(x / y);
 console.log(x % y);
 console.log(x ** y);
+//Pre-increment
+console.log(x);
+console.log(++x);
+console.log(x);
+console.log(++x);
+console.log(x);
+console.log(++x);
+console.log(x);
+//Post-increment
+console.log(x++);
+console.log(x);
+console.log(x++);
+console.log(x);
+console.log(x++);
+console.log(x);
+console.log(x++);
 
-console.log(x);
-console.log(++x);
-console.log(x);
-console.log(++x);
-console.log(x);
-console.log(++x);
-console.log(x);
+
+let r = 10;
+let s = 6;
+
+console.log("pre")
+
+r = --s;
+console.log(r);
+console.log(s);
+
+
+console.log("post")
+
+r = s--;
+console.log(r)
+console.log(s)
